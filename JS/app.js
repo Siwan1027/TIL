d0e2aa98f00b2e56cbd561bcc63f499336879a10
Siwan1027 <wogns3465@gmail.com>
2023-09-28 16:32:30 +0900

    learn setter and getter

--- JS/app.js
+++ JS/app.js
@@ -39,3 +39,77 @@ function printThree({alias, name, actor}) {
 
 printThree(ironMan)
 printThree(captainAmerica)
+
+// 객체와 함수
+const car = {
+    name :'자동차',
+    sound:'부릉부릉',
+    drive: function drive() {
+        console.log(this.sound)
+    }
+};
+
+car.drive();
+
+// 객체와 함수
+const motorcycle = {
+    name :'오토바이',
+    sound:'부와아앙',
+    // 객체 내 함수를 선언할 시 이름을 정하지 않아도 가능
+    ride: function() {
+        console.log(this.sound)
+    }
+};
+
+motorcycle.ride();
+
+// getter & setter
+const numbers = {
+    a:1,
+    b:2,
+    // 여기서 get 으로 함수를 선언했기 때문에
+    get sum() {
+        console.log('sum 함수가 실행됨');
+        return this.a + this.b;
+    }
+};
+// 
+console.log(numbers.sum);
+numbers.b = 5;
+console.log(numbers.sum);
+// 이 명령어는 실행이 안됨
+// console.log(numbers.sum());
+
+const nums = {
+    _a : 1,
+    _b : 2,
+    sum : 3,
+    calculate() {
+        console.log('calculate');
+        this.sum = this._a + this._b;
+    },
+    get a() {
+        return this._a;
+    },
+    get b() {
+        return this._b;
+    },
+    set a(value) {
+        console.log(`a가 ${value} 로 바뀝니다.`);
+        this._a = value;
+        this.calculate();
+    },
+    set b(value) {
+        console.log(`b가 ${value} 로 바뀝니다.`);
+        this._a = value;
+        this.calculate();
+    }
+};
+
+console.log(nums.sum);
+nums.a = 5;
+nums.b =7;
+nums.a = 9;
+console.log(nums.sum);
+console.log(nums.sum);
+console.log(nums.sum);
